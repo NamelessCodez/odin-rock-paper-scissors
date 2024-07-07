cd7e89f29f440f03b07fb2a4b0597c6d1388f868
Satyam <satyamsharma2106@gmail.com>
2024-07-07 10:17:19 +0530

    add logic to play a single round

--- script.js
+++ script.js
@@ -1,22 +1,84 @@
-//To get computer choice
+//GET computer choice
 function getComputerChoice() {
-//initialise variable for computer choice   
+//INITIALIZE variable for computer choice   
     let computerChoice;
-//Get a random integer between 0 and 3 (excluding 3) by multiplying output of math.random by 3 and truncating it
-let randInt = Math.floor (Math.random() * 3);
+//GET a random integer between 0 and 3 (excluding 3) by multiplying output of math.random by 3 and truncating it
+    let randInt = Math.floor (Math.random() * 3);
 //IF random int is 0 then computer chooses rock
-if (randInt === 0) {
-    computerChoice = 'rock';
-}
+    if (randInt === 0) {
+        computerChoice = 'rock';
+    }
 //ELSE IF random int is 1 then computer chooses paper
-else if (randInt === 1) {
-    computerChoice = 'paper';
-}
-//ELSE random int is 2 then computer chooses scissors
-else {
-    computerChoice = 'scissors';
-}
-return computerChoice;
+    else if (randInt === 1) {
+        computerChoice = 'paper';
+    }
+//ELSE computer chooses scissors
+    else {
+        computerChoice = 'scissors';
+    }
+    return computerChoice;
 }
 
-console.log(getComputerChoice());
+// GET Player choice
+function getPlayerChoice() {
+    playerChoice = prompt("Enter your choice");
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+        return playerChoice;
+    } else {
+        console.log('Invalid input');
+        return null;
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0; 
+
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    let winner = checkWinner(playerChoice, computerChoice);
+    if (winner === 'Player') {
+        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+        playerScore++;
+    } else if ( winner === 'Computer') {
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+        computerScore++;
+    } else {
+        console.log(`It's a tie!`);
+    }
+    console.log(winner);
+    console.log(playerChoice);
+    console.log(computerChoice);
+}
+playRound();
+
+function checkWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return;
+    } else {
+        if (playerChoice === 'rock') {
+            switch (computerChoice) {
+                case 'paper':
+                    return 'Computer';
+                case 'scissors':
+                    return 'Player';
+            }
+        } else if (playerChoice === 'paper') {
+            switch (computerChoice) {
+                case 'rock':
+                    return 'Player';
+                case 'scissors':
+                    return 'Computer';       
+            }
+        } else {
+            switch (computerChoice) {
+                case 'rock':
+                    return 'Computer';
+                case 'paper':                  
+                    return 'Player';
+            }
+        }
+    }
+}
+
